ece1f0369409ce0c18ac835a82c685485c0aba13
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-13 06:22:45 +0000

    add react-hook-form and zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const schema = z.object({
+    name: z.string().min(1, { message: 'Required' }),
+    age: z.number().min(10),
+  });
 
+  
+  const {
+    register,
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(schema),
+  })
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
